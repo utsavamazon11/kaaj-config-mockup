3fdc0f45005542d0d1daba436fce57fe3077f0f7
utsavamazon11 <123217343+utsavamazon11@users.noreply.github.com>
2023-06-06 18:34:08 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+// script.js
+$(function () {
+  $('[data-bs-toggle="tooltip"]').tooltip();
+});
+
+$(document).ready(function () {
+  $('#configForm').submit(function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(this);
+    const data = {};
+
+    for (const [key, value] of formData) {
+      data[key] = value;
+    }
+
+    const yamlData = jsyaml.safeDump(data);
+
+    const yamlBlob = new Blob([yamlData], { type: 'text/yaml' });
+    const url = URL.createObjectURL(yamlBlob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'config.yaml';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  });
+});
