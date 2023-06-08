77e7cdcb6bfec906f9b58049219f842759b46d3e
utsavamazon11 <123217343+utsavamazon11@users.noreply.github.com>
2023-06-07 18:08:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,32 @@
-$(function () {
-  $('[data-bs-toggle="tooltip"]').tooltip();
-});
-
 $(document).ready(function () {
   $('#configForm').on('submit', function (event) {
     event.preventDefault();
 
-    const collectorFrequency = document.getElementById('collectorFrequency').value;
-    const collectorFrequencyUnit = document.getElementById('collectorFrequencyUnit').value;
-    const collectorRetries = document.getElementById('collectorRetries').value;
-    const collectorSchema = document.getElementById('collectorSchema').value;
-    const collectorTableName = document.getElementById('collectorTableName').value;
+    const formData = new FormData(this);
+    const data = {};
 
-    if (!collectorFrequency || !collectorRetries || !collectorSchema || !collectorTableName) {
-      alert('Please fill in all required fields in the Collector section.');
-      return;
+    for (const [key, value] of formData) {
+      data[key] = {
+        name: key,
+        value: value
+      };
     }
-
-    const data = {
-      'Collector': {
-        'Frequency': collectorFrequency + ' ' + collectorFrequencyUnit,
-        'Number of retries': collectorRetries,
-        'Destination': {
-          'Schema': collectorSchema,
-          'Table Name': collectorTableName
-        }
-      }
-    };
 
     const yamlData = jsyaml.safeDump(data);
 
+    // Create a Blob object with the YAML data
     const yamlBlob = new Blob([yamlData], { type: 'text/yaml' });
-    const url = URL.createObjectURL(yamlBlob);
+
+    // Create a temporary anchor element
     const a = document.createElement('a');
-    a.href = url;
+    a.href = URL.createObjectURL(yamlBlob);
     a.download = 'config.yaml';
+
+    // Programmatically trigger the download event
     a.click();
+
+    // Clean up the URL object
+    URL.revokeObjectURL(a.href);
   });
 });
 
@@ -43,47 +34,73 @@ function nextSlider(currentSliderId, nextSliderId) {
   const currentSlider = document.getElementById(currentSliderId);
   const nextSlider = document.getElementById(nextSliderId);
 
-  currentSlider.style.display = 'none';
-  nextSlider.style.display = 'block';
+  currentSlider.style.display = "none";
+  nextSlider.style.display = "block";
 }
 
 function previousSlider(currentSliderId, previousSliderId) {
   const currentSlider = document.getElementById(currentSliderId);
   const previousSlider = document.getElementById(previousSliderId);
 
-  currentSlider.style.display = 'none';
-  previousSlider.style.display = 'block';
+  currentSlider.style.display = "none";
+  previousSlider.style.display = "block";
 }
 
 function saveConfig() {
+  const collectorApiKey = document.getElementById('collectorApiKey').value;
   const collectorFrequency = document.getElementById('collectorFrequency').value;
   const collectorFrequencyUnit = document.getElementById('collectorFrequencyUnit').value;
   const collectorRetries = document.getElementById('collectorRetries').value;
   const collectorSchema = document.getElementById('collectorSchema').value;
   const collectorTableName = document.getElementById('collectorTableName').value;
 
-  if (!collectorFrequency || !collectorRetries || !collectorSchema || !collectorTableName) {
+  if (!collectorApiKey || !collectorFrequency || !collectorRetries || !collectorSchema || !collectorTableName) {
     alert('Please fill in all required fields in the Collector section.');
     return;
   }
 
   const data = {
-    'Collector': {
-      'Frequency': collectorFrequency + ' ' + collectorFrequencyUnit,
-      'Number of retries': collectorRetries,
-      'Destination': {
-        'Schema': collectorSchema,
-        'Table Name': collectorTableName
-      }
+    collectorApiKey: {
+      name: 'collectorApiKey',
+      value: collectorApiKey
+    },
+    collectorFrequency: {
+      name: 'collectorFrequency',
+      value: collectorFrequency
+    },
+    collectorFrequencyUnit: {
+      name: 'collectorFrequencyUnit',
+      value: collectorFrequencyUnit
+    },
+    collectorRetries: {
+      name: 'collectorRetries',
+      value: collectorRetries
+    },
+    collectorSchema: {
+      name: 'collectorSchema',
+      value: collectorSchema
+    },
+    collectorTableName: {
+      name: 'collectorTableName',
+      value: collectorTableName
     }
   };
 
   const yamlData = jsyaml.safeDump(data);
 
+  // Create a Blob object with the YAML data
   const yamlBlob = new Blob([yamlData], { type: 'text/yaml' });
-  const url = URL.createObjectURL(yamlBlob);
+
+  // Create a temporary anchor element
   const a = document.createElement('a');
-  a.href = url;
+  a.href = URL.createObjectURL(yamlBlob);
   a.download = 'config.yaml';
+
+  // Programmatically trigger the download event
   a.click();
+
+  // Clean up the URL object
+  URL.revokeObjectURL(a.href);
+
+  alert('Configuration saved!');
 }
