b73b88731b7853fc36ca8ba9d3d7728345d84898
utsavamazon11 <123217343+utsavamazon11@users.noreply.github.com>
2023-06-07 17:54:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,28 @@ $(document).ready(function () {
   $('#configForm').on('submit', function (event) {
     event.preventDefault();
 
-    const formData = new FormData(this);
-    const data = {};
+    const collectorFrequency = document.getElementById('collectorFrequency').value;
+    const collectorFrequencyUnit = document.getElementById('collectorFrequencyUnit').value;
+    const collectorRetries = document.getElementById('collectorRetries').value;
+    const collectorSchema = document.getElementById('collectorSchema').value;
+    const collectorTableName = document.getElementById('collectorTableName').value;
 
-    for (const [key, value] of formData) {
-      data[key] = value;
+    if (!collectorFrequency || !collectorRetries || !collectorSchema || !collectorTableName) {
+      alert('Please fill in all required fields in the Collector section.');
+      return;
     }
+
+    const data = {
+      'Collector': {
+        'Frequency': collectorFrequency + ' ' + collectorFrequencyUnit,
+        'Number of retries': collectorRetries,
+        'Destination': {
+          'Schema': collectorSchema,
+          'Table Name': collectorTableName
+        }
+      }
+      // Add Transformer and Analytics sections if needed
+    };
 
     const yamlData = jsyaml.safeDump(data);
 
@@ -27,17 +43,17 @@ $(document).ready(function () {
 function nextSlider(currentSliderId, nextSliderId) {
   const currentSlider = document.getElementById(currentSliderId);
   const nextSlider = document.getElementById(nextSliderId);
-  
-  currentSlider.style.display = "none";
-  nextSlider.style.display = "block";
+
+  currentSlider.style.display = 'none';
+  nextSlider.style.display = 'block';
 }
 
 function previousSlider(currentSliderId, previousSliderId) {
   const currentSlider = document.getElementById(currentSliderId);
   const previousSlider = document.getElementById(previousSliderId);
-  
-  currentSlider.style.display = "none";
-  previousSlider.style.display = "block";
+
+  currentSlider.style.display = 'none';
+  previousSlider.style.display = 'block';
 }
 
 function saveConfig() {
@@ -52,7 +68,24 @@ function saveConfig() {
     return;
   }
 
-  // Retrieve and process the form data here
-  // Store the configuration as a YAML file or perform any other necessary actions
-  alert('Configuration saved!');
+  const data = {
+    'Collector': {
+      'Frequency': collectorFrequency + ' ' + collectorFrequencyUnit,
+      'Number of retries': collectorRetries,
+      'Destination': {
+        'Schema': collectorSchema,
+        'Table Name': collectorTableName
+      }
+    }
+    // Add Transformer and Analytics sections if needed
+  };
+
+  const yamlData = jsyaml.safeDump(data);
+
+  const yamlBlob = new Blob([yamlData], { type: 'text/yaml' });
+  const url = URL.createObjectURL(yamlBlob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = 'config.yaml';
+  a.click();
 }
